e9ee93311134c15065d5744f253507aea8a57297
Artemis <chunliu89@outlook.com>
2023-10-20 14:46:20 -0400

    testing referral2

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ReferralSignupForm from "./components/Referral";
+// import ReferralSignupForm from "./components/Referral";
+import ReferralSignupForm from './components/Referrals2';
 import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 
